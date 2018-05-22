7ec79b5d70fa05fb63ece29181b6daf83691da9a
Christian Ruigrok <christian@ruigrok.info>
2018-05-22 10:30:29 +0200

    Add animation to change theme and add analytics

--- script.js
+++ script.js
@@ -2,16 +2,61 @@ let darkTheme = window.localStorage.getItem('theme') === 'dark' ? true : false;
 
 function toggleTheme() {
   darkTheme = !darkTheme;
-  updateTheme();
+  updateTheme(true);
   window.localStorage.setItem('theme', darkTheme ? 'dark' : 'light');
 }
 
-function updateTheme() {
-  if (darkTheme) {
-    document.body.classList.add('dark');
-  } else {
-    document.body.classList.remove('dark');
+function updateTheme(fromToggle) {
+  const diameterValue = (Math.sqrt(Math.pow(window.outerHeight, 2) + Math.pow(window.outerWidth, 2)) * 2);
+
+  const hasClassDark = document.body.classList.contains('dark');
+  if ((darkTheme && hasClassDark) || (!darkTheme && !hasClassDark)) {
+    return;
   }
+
+  if (fromToggle === true) {
+    initAnimate(diameterValue, darkTheme);
+  } else {
+    if (darkTheme) {
+      document.body.classList.add('dark');
+    }
+  }
+}
+
+function initAnimate(diameter, toColor) {
+  const animationCircle = document.querySelector('.animation-circle');
+  animationCircle.style.opacity = 1;
+  animationCircle.style.backgroundColor = toColor ? '#000' : '#fff';
+
+  let current = 20;
+  return (function animate() {
+    if (current > diameter) {
+      animationCircle.style.opacity = 0;
+
+      if (darkTheme) {
+        if (document.body.classList.contains('dark')) {
+          return;
+        }
+
+        document.body.classList.add('dark');
+      } else {
+        if (!document.body.classList.contains('dark')) {
+          return;
+        }
+
+        document.body.classList.remove('dark');
+      }
+    }
+
+    current += (current * current) / 800;
+
+    animationCircle.style.height = current + 'px';
+    animationCircle.style.width = current + 'px';
+    animationCircle.style.top = -(current / 2) + 34 + 'px';
+    animationCircle.style.right = -(current / 2) + 34 + 'px';
+
+    requestAnimationFrame(animate);
+  })();
 }
 
 document.querySelector('.theme-toggle').addEventListener('click', toggleTheme);

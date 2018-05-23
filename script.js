ee7674ce59051b8c7579ff78c649242409c60959
Christian Ruigrok <christian@ruigrok.info>
2018-05-23 11:21:30 +0200

    Start work on projects

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let darkTheme = window.localStorage.getItem('theme') === 'dark' ? true : false;
 
-function toggleTheme() {
-  darkTheme = !darkTheme;
+function toggleTheme(dark) {
+  darkTheme = dark === undefined ? !darkTheme : dark;
   updateTheme(true);
   window.localStorage.setItem('theme', darkTheme ? 'dark' : 'light');
 }
@@ -59,5 +59,76 @@ function initAnimate(diameter, toColor) {
   })();
 }
 
-document.querySelector('.theme-toggle').addEventListener('click', toggleTheme);
-document.addEventListener("DOMContentLoaded", updateTheme);
+function init() {
+  updateTheme();
+  createDots();
+}
+
+document.querySelector('.theme-toggle svg').addEventListener('click', toggleTheme);
+document.addEventListener("DOMContentLoaded", init);
+
+let currentSlide = 0;
+let stopInterval = false;
+const slides = Array.from(document.querySelectorAll('.project-slide'));
+function updateSlide(index, interval) {
+  currentSlide = index % slides.length;
+
+  if (interval === true && stopInterval === true) {
+    return;
+  }
+
+  Array.from(document.querySelectorAll('.dot')).forEach(function(dot, dotIndex) { 
+    if (dotIndex === currentSlide) {
+      dot.classList.add('active');
+    } else {
+      dot.classList.remove('active');
+    }
+  });
+
+  slides.forEach(function(slide, slideIndex) {
+    if (slideIndex !== currentSlide) {
+      slide.classList.add('hidden');
+    } else {
+      slide.classList.remove('hidden');
+    }
+  });
+}
+
+function createDots() {
+  const controls = document.querySelector('.project-controls');
+  const slides = Array.from(document.querySelectorAll('.project-slide'));
+
+  slides.forEach(function(slide, index) {
+    const dot = document.createElement('button');
+    dot.className = index === 0 ? 'dot active' : 'dot';
+    dot.addEventListener('click', function() {
+      stopInterval = true;
+      return updateSlide(index);
+    });
+    controls.appendChild(dot);
+  });
+}
+
+setInterval(function() {
+  updateSlide(currentSlide + 1, true);
+}, 10000);
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 39 && currentSlide < slides.length - 1) {
+    stopInterval = true;
+    updateSlide(currentSlide + 1);
+  }
+
+  if (event.keyCode === 37 && currentSlide > 0) {
+    stopInterval = true;
+    updateSlide(currentSlide - 1);
+  }
+
+  if (event.keyCode === 78) {
+    toggleTheme(true);
+  }
+
+  if (event.keyCode === 68) {
+    toggleTheme(false);
+  }
+})

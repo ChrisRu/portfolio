3b2747f8d2d201e5ff123e0decef9ee9bf5c9eb3
Christian Ruigrok <christian@ruigrok.info>
2018-05-28 15:24:02 +0200

    Add smoothscroll file

---
+++ src/scripts/smoothScroll.js
@@ -0,0 +1,42 @@
+function ease(time, from, to, duration) {
+  time /= duration / 2;
+  if (time < 1) {
+    return to / 2 * time * time + from;
+  } else {
+    t--;
+    return -to / 2 * (time * (time - 2) - 1) + from;
+  }
+}
+
+function smoothScroll(targetElement, duration = 800, easing = null) {
+  const startPositionY = window.pageYOffset;
+  const startPositionX = window.pageXOffset;
+  const targetPositionY = target.getBoundingClientRect().top;
+  const targetPositionX = target.getBoundingClientRect().left;
+
+  const distanceX = targetPositionX - startPositionX;
+  const distanceY = targetPositionY - startPositionY;
+
+  let startTime;
+
+  function animate(currentTime) {
+    if (startTime === undefined) {
+      startTime = currentTime;
+    }
+
+    const elapsed = currentTime - startTime;
+
+    window.scrollTo(
+      (easing || ease)(elapsed, startPositionX, distanceX, duration),
+      (easing || ease)(elapsed, startPositionY, distanceY, duration)
+    );
+
+    if (elapsed < duration) {
+      requestAnimationFrame(animate);
+    }
+  }
+
+  requestAnimationFrame(animation);
+}
+
+export default smoothScroll;
